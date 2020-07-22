8044092fbdbf5fddc22998f4767828408b119797
levy225 <levy.zire@gmail.com>
2020-07-22 21:04:34 +0200

    feat(authentification): Add pdo connexion to bdd and simple authentification

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    try {
+
+        $bdd = new PDO('mysql:dbname=boutique;host=localhost:3308','root', '');
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $req = "SELECT * FROM user WHERE (username = '".$username."' AND password = '".$password."')";
+        $res = $bdd->query($req);
+
+        $user = $res->fetch();
+
+        if($user) {
+
+            session_start();
+            $_SESSION['id'] = $user[0];
+            $_SESSION['username'] = $user[1];
+
+            header("Location:/Boutique/");
+        } else {
+            header("Location:/Boutique/connexion.html?error=1");
+        }
+
+    } catch (PDOException $e) {
+        echo "Une erreur s'est produite " . $e->getMessage();
+    }
+
+?>
