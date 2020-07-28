4b077e3c4cafe55b6d328792b40a41e216dc57f3
Francoise1999 <lizmentor15@gmail.com>
2020-07-28 12:59:11 -0400

    Connect page connexio.html at the database "boutique"

--- login.php
+++ login.php
@@ -1,11 +1,17 @@
 <?php
+    /* try{
+        $base = new PDO('mysql:host=localhost', 'root', '');
+        $base->exec("CREATE DATABASE utilisateurs DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+    }catch (PDOException $e) {
+        echo "Une erreur s'est produite " . $e->getMessage();
+    } */
 
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     try {
 
-        $bdd = new PDO('mysql:dbname=boutique;host=localhost:3308','root', '');
+        $bdd = new PDO('mysql:dbname=boutique;host=localhost:3306','root', '');
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $req = "SELECT * FROM user WHERE (username = '".$username."' AND password = '".$password."')";
@@ -19,9 +25,9 @@
             $_SESSION['id'] = $user[0];
             $_SESSION['username'] = $user[1];
 
-            header("Location:/Boutique/");
+            header("Location:/Boutique/Boutique/");
         } else {
-            header("Location:/Boutique/connexion.html?error=1");
+            header("Location:/Boutique/Boutique/connexion.html?error=1");
         }
 
     } catch (PDOException $e) {
